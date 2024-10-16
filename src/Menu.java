f344be388ac5c3c29f659837312da0650409ac07
DJmoos3 <isaac.strandh@gmail.com>
2024-10-16 10:07:47 +0200

    Added catalogue and product to start the menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,6 +1,43 @@
+import java.util.Scanner;
+
 public class Menu {
 
+    Scanner scanner = new Scanner(System.in);
+
+    String answer;
+    String name;
+    int price;
+
+    Catalog catalog = new Catalog();
+
     public void run() {
+
+        System.out.println("Hello what do you wanna do with the products?");
+        System.out.println("1: Add new product\n2: Show whole catalogue\n3: Remove Product");
+
+        answer = scanner.nextLine();
+
+        if (answer.equals("1")) {
+            System.out.println("What product do you want to add?");
+            name = scanner.nextLine();
+            System.out.println("And what is the price?");
+            if(scanner.hasNextInt())
+            {
+
+            }
+
+            catalog.addProduct(new Product(name, price));
+
+        } else if (answer.equals("2")) {
+            catalog.showAllProducts();
+
+        }/* else if (answer.equals("3")) {
+            catalog.deleteProduct();
+
+        }*/ else {
+            System.out.println("Wrong input");
+        }
+
 
 
     }

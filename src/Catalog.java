1b8eb4ce6ed7f8d816c182671561b41e28d11877
robinjakobsson <3ggapp24.jakoro@folkuniversitetet.nu>
2024-10-16 09:51:08 +0200

    Created methods

--- src/Catalog.java
+++ src/Catalog.java
@@ -1,5 +1,35 @@
 import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * Class to hold the products
+ */
 public class Catalog {
-   
+   protected List<Product> productList = new ArrayList<>();
+
+
+   /**
+    * Method to add a product
+    * @param product Is the product
+    */
+   public void addProduct (Product product){
+      productList.add(product);
+   }
+
+   /**
+    * Method to delete a product
+    * @param product is the product
+    */
+   public void deleteProduct(Product product){
+      productList.remove(product);
+   }
+
+   /**
+    * Method to show all products
+    */
+   public void showAllProducts(){
+      for (Product p : productList){
+         System.out.println(p.toString());
+      }
+   }
 }
